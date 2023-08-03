e3db81440a99d31c405f5a3b87f48a258c336268
jgabrielmedina <jogabrielmedina@gmail.com>
2023-08-03 20:48:05 -0300

    add weather app

--- src/components/listProyect.jsx
+++ src/components/listProyect.jsx
@@ -12,13 +12,13 @@ function ListProyect() {
                 <h1 className="aboutme">PROYECTOS</h1>
 
              
-
                 <Proyect
-                    name= "Hello!"
-                    description = " E-commerce realizado en conjunto con un equipo durante la cursada de DigitalHouse"
-                    url = "https://github.com/sabriw88/grupo_6_panaderiaHello"
-                    img = "/PHOTO-2023-06-29-12-23-09.jpg"
-                    tech = {<DiNodejs></DiNodejs>}
+                    name= "Weather App"
+                    description = "Mediante el consumo de 3 diferentes APIs se realizo una app que te brinda el reporte del clima segun el pais y la ciudad seleccionada."
+                    url = "https://github.com/jgabrielmedina/weatherApp"
+                    img = "weatherApp.png"
+                    tech = {<BiLogoReact></BiLogoReact>}
+                    demo = "https://weather-app-self-omega.vercel.app/"
                 />
                 <Proyect
                     name= "To Do List"
@@ -28,6 +28,14 @@ function ListProyect() {
                     tech = {<BiLogoReact></BiLogoReact>}
                     demo = "https://to-do-list-drab-pi.vercel.app/"
                 />
+                <Proyect
+                    name= "Hello!"
+                    description = " E-commerce realizado en conjunto con un equipo durante la cursada de DigitalHouse"
+                    url = "https://github.com/sabriw88/grupo_6_panaderiaHello"
+                    img = "/PHOTO-2023-06-29-12-23-09.jpg"
+                    tech = {<DiNodejs></DiNodejs>}
+                />
+              
 
                
             </div>
