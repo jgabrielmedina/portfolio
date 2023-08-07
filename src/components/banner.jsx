48009f09c1547b841349b7b430d6966918649659
jgabrielmedina <jogabrielmedina@gmail.com>
2023-08-07 11:40:12 -0300

    adjuntando el CV

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -14,7 +14,10 @@ function Banner() {
                 <div className="bannerName">
                 <h1>FRONT-END REACT DEVELOPER</h1>
                 <p className="subtitulo">Jose Gabriel Medina</p>
+                <p className="linkCv"><a href="https://drive.google.com/file/d/1DUmxraO1U46EfmAQlpelU-eLIsUELTFy/view?usp=drive_link"target="_blank"  rel="noreferrer" >Curriculum Vitae</a></p>
                 </div>
+          
+
             </div>
         </React.Fragment>
 
